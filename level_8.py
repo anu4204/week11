d2c36755f1083e47022a6d4f264792d150927244
Anu Anujin <anuanujin@Anus-Mac.local>
2025-04-08 19:49:02 -0400

    Level 8 complete

---
+++ level_8.py
@@ -0,0 +1,12 @@
+
+
+# 1. What happens when no prompt is given to the input function?
+print('Q1: No prompt in input')
+x = input()
+print('Hello, ' + x)
+
+# 2. Is it possible to pass a variable into the input function instead of a string?
+print('Q2:  Using a variable as the prompt')
+prompt="Favourite season"
+x = input(prompt)
+print('Hello, ' + x)
