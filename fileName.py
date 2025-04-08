94a79656d8f252a2f510e838fbf55c8a8fcb43e0
Anu Anujin <anuanujin@Anus-Mac.local>
2025-04-08 18:59:44 -0400

    Level 5 Complete

--- fileName.py
+++ fileName.py
@@ -8,3 +8,16 @@ f = None # The variable called f is now holding an absense of value
 age = 32 # The variable called age is now holding the integer value of 32
 name = "Insert your name here" # The variable called name is now holding the string value of "Insert your name here"
 instrument = "Insert your instrument here" # The variable called instrument is now holding the string value of "Insert your instrument here"
+
+'''
+Rules for Python variables:
+
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+
+$$$Variable = “bad”
+
+'''
